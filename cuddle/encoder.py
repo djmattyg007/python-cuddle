cf019a3f10f0c9be7dc1c98bf59b687003197ffa
Matthew Gamble <git@matthewgamble.net>
2021-09-25 17:29:23 +1000

    Output type annotations in encoder
    We're still not decoding them though.

--- cuddle/encoder.py
+++ cuddle/encoder.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Callable, Iterable, Optional, Union
+from typing import Any, Callable, Iterable, Optional, Tuple, Union
 
 import regex
 
@@ -8,7 +8,7 @@ from ._escaping import named_escape_inverse
 from .structure import Document, Node
 
 
-DefaultHandler = Callable[[Any], str]
+DefaultHandler = Callable[[Any], Tuple[Optional[str], str]]
 
 ident_re = regex.compile(
     r'^[^\\<{;\[=,"0-9\t \u00A0\u1680\u2000-\u200A\u202F\u205F\u3000\uFFEF\r\n\u0085\u000C\u2028\u2029][^\\;=,"\t \u00A0\u1680\u2000-\u200A\u202F\u205F\u3000\uFFEF\r\n\u0085\u000C\u2028\u2029]*$'
@@ -45,8 +45,12 @@ def _make_encoder(_indent: str, _default: DefaultHandler) -> Callable[[Document]
             return _intstr(val)
         elif isinstance(val, float):
             return _floatstr(val)
+
+        value_type, value_string = _default(val)
+        if value_type:
+            return f"({value_type}){format_string(value_string)}"
         else:
-            return _default(val)
+            return format_string(value_string)
 
     def format_node(node: Node, /, *, top_level: bool = False) -> Iterable[str]:
         if top_level:
@@ -78,39 +82,48 @@ def _make_encoder(_indent: str, _default: DefaultHandler) -> Callable[[Document]
     return format_document
 
 
-def extended_default(o: Any) -> str:
+def extended_default(o: Any) -> Tuple[Optional[str], str]:
     from base64 import b64encode
     from datetime import date, datetime, time
     from decimal import Decimal
     from ipaddress import IPv4Address, IPv6Address
     from re import Pattern as RePattern
+    from urllib.parse import DefragResult as UrlDefragResult
+    from urllib.parse import ParseResult as UrlParseResult
+    from urllib.parse import SplitResult as UrlSplitResult
     from uuid import UUID
 
     if isinstance(o, bytes):
-        return b64encode(o).decode("utf-8")
+        return "base64", b64encode(o).decode("utf-8")
     elif isinstance(o, datetime):
-        return o.isoformat()
+        return "date-time", o.isoformat()
     elif isinstance(o, date):
-        return o.isoformat()
+        return "date", o.isoformat()
     elif isinstance(o, time):
-        return o.isoformat()
+        return "time", o.isoformat()
     elif isinstance(o, Decimal):
-        return str(o)
-    elif isinstance(o, (IPv4Address, IPv6Address)):
-        return str(o)
-    elif isinstance(o, RePattern):
-        return o.pattern
-    elif isinstance(o, regex.Pattern):
-        return o.pattern
+        return "decimal", str(o)
+    elif isinstance(o, IPv4Address):
+        return "ipv4", str(o)
+    elif isinstance(o, IPv6Address):
+        return "ipv6", str(o)
+    elif isinstance(o, (RePattern, regex.Pattern)):
+        return "regex", o.pattern
+    elif isinstance(o, (UrlDefragResult, UrlParseResult, UrlSplitResult)):
+        return "url", o.geturl()
     elif isinstance(o, UUID):
-        return str(o)
+        return "uuid", str(o)
 
     raise TypeError(f"Object of type {o.__class__.__name__} is not KDL serializable")
 
 
 class KDLEncoder:
     def __init__(
-        self, *, indent: Union[str, int, None] = None, default: Optional[DefaultHandler] = None
+        self,
+        *,
+        indent: Union[str, int, None] = None,
+        default: Optional[DefaultHandler] = None,
+        use_extended_default: bool = True,
     ):
         self.indent: str
         if isinstance(indent, str):
@@ -122,8 +135,10 @@ class KDLEncoder:
 
         if default is not None:
             self.default = default
+        elif use_extended_default:
+            self.default = extended_default
 
-    def default(self, o: Any) -> str:
+    def default(self, o: Any) -> Tuple[Optional[str], str]:
         raise TypeError(f"Object of type {o.__class__.__name__} is not KDL serializable")
 
     def encode(self, doc: Document) -> str:
